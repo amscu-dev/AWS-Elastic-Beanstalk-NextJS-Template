93e79cc453fe7195b77f5297c9bda0ebd6663d45
amscu-dev <amarinescu.dev@gmail.com>
2026-04-02 23:14:30 +0300

    ci(main/.github): add workflow for auto deploy eb environment

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,10 +5,6 @@ export const Env = createEnv({
   server: {
     FLAGSMITH_KEY: z.string().min(1),
   },
-  client: {
-    NEXT_PUBLIC_APP_URL: z.url(),
-  },
-  experimental__runtimeEnv: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-  },
+  client: {},
+  experimental__runtimeEnv: {},
 });
